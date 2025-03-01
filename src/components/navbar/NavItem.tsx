d072a85882ea82d9b9b9f92379d3a032408d8dce
Nuran Mirzayev <nuranm@noviopus.com>
2025-03-01 19:49:59 +0200

    correct build

--- src/components/navbar/NavItem.tsx
+++ src/components/navbar/NavItem.tsx
@@ -10,22 +10,24 @@ interface NavItemProps {
 
 const NavItem: React.FC<NavItemProps> = ({ item, textColor }) => {
 	return (
-		<Button
-			component={Link}
-			to={item.route}
-			sx={{
-				display: { xs: 'none', md: 'flex' },
-				marginRight: '20px',
-				color: textColor, // Теперь цвет текста динамический
-				fontSize: '13px',
-				'&:last-child': {
-					marginRight: 0,
-					paddingRight: 0,
-				},
-			}}
-		>
-			{item.title}
-		</Button>
+		<>
+			<Button
+				component={Link}
+				to={item.route}
+				sx={{
+					display: { xs: 'none', md: 'flex' },
+					marginRight: '20px',
+					color: textColor,
+					fontSize: '13px',
+					'&:last-child': {
+						marginRight: 0,
+						paddingRight: 0,
+					},
+				}}
+			>
+				{item.title}
+			</Button>
+		</>
 	)
 }
 
